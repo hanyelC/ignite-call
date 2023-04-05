8a0a2046372ee43056c4af6897e49af7905bdc54
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-04-05 10:51:04 -0300

    feat: register form

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,4 +1,4 @@
-import { Container, Form, Header } from './styles'
+import { Container, Form, FormError, Header } from './styles'
 
 import {
   Button,
@@ -8,8 +8,38 @@ import {
   TextInput,
 } from '@hanyel-ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 caracteres' })
+    .regex(/^([a-z\\\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens',
+    })
+    .transform((username) => username.toLowerCase()),
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras' }),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  async function handleRegister(data: RegisterFormData) {
+    console.log(data)
+  }
+
   return (
     <Container>
       <Header>
@@ -21,7 +51,7 @@ export default function Register() {
 
         <MultiStep as="div" size={4} currentStep={1} />
 
-        <Form as="form">
+        <Form as="form" onSubmit={handleSubmit(handleRegister)}>
           <label>
             <Text size="sm">Nome de usuário</Text>
             <TextInput
@@ -29,7 +59,11 @@ export default function Register() {
               prefix="ignite.com/"
               placeholder="seu-usuário"
               variants={{ size: 'md' }}
+              {...register('username')}
             />
+            {errors.username && (
+              <FormError size="sm">{errors.username.message}</FormError>
+            )}
           </label>
 
           <label>
@@ -38,9 +72,14 @@ export default function Register() {
               as="input"
               placeholder="Seu nome"
               variants={{ size: 'md' }}
+              {...register('name')}
             />
+
+            {errors.name && (
+              <FormError size="sm">{errors.name.message}</FormError>
+            )}
           </label>
-          <Button>
+          <Button type="submit" disabled={isSubmitting}>
             Próximo passo
             <ArrowRight />
           </Button>
